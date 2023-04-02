0f0db9d228fec85d5c84602ed2169f047fc9c0a9
Ebenenstil <Kontakt@davidknospe.de>
2023-04-02 16:55:44 +0200

    Eingabe mit Datenauslesen und Speichern erweitert

--- Fenstertest.py
+++ Fenstertest.py
@@ -1,8 +1,12 @@
-from tkinter import *
+from tkinter import * #Import für Fenstersteuerung
+import datetime as dt #Import für Datumsfunktion
+import json           #Import für Datenspeicherung : Json
 
+#Funktion zum Umrechnen von Fahrzeit zu Schritten
 def umrechnung(eingabe):
     return(float(eingabe)*125)
 
+#Funktion um den Prozentsatz zu 10k zu berechnen und bewertung auszugeben
 def prozentsatz(input):
     rechnung = (float(input) / 10000) * 100
     if rechnung > 100 :
@@ -10,6 +14,7 @@ def prozentsatz(input):
     else:
         return("Damit hast du dein Tagesziel zu: "+ str(rechnung) + " % erreicht!")
 
+#Funktion für Knopf-Drücken
 def button_action():
     entry_text = eingabefeld.get()
     schritte = umrechnung(eingabefeld.get())
@@ -19,35 +24,46 @@ def button_action():
     else:
         entry_text = "Du hast umgerechnet ganze: " + str(schritte)+ " Schritte mit dem Fahrrad erreicht!\n " +prozentsatz(schritte)
         ausgabefeld.config(text=entry_text)
+    gesamt = float(datenbank[str(dt.date.today())]) + schritte
+    datenbank[str(dt.date.today())] = str(gesamt)
 
-#Aussehen wird bestimmt
+    with open("data.json","w") as b:
+        json.dump(datenbank,b)
 
+#Dictornary Datenbank wird aus JSON Datei eingelesen oder als leeres Dict.erstellt
+try:
+    with open("data.json","r") as f:
+        datenbank = json.load(f)
+except:
+    datenbank ={}
+
+#Parameter für die Optik des Fensters wird bestimmt
 farbstil = "white"
 fenster = Tk()
 fenster.iconbitmap("bike.ico")
 fenster.configure(bg=farbstil)
 fenster.title("Minuten auf dem Fahrrad -> Schritte")
 
-
 # Anweisungs-Label
 my_label = Label(fenster, text="Wie lange bist du gefahren ? ",background=farbstil)
 
-# In diesem Label wird nach dem Klick auf den Button der Benutzer
-# mit seinem eingegebenen Namen begrüsst.
+# Ausgabefeld wo die Ergebnisse der Berechnungen gezeigt werden
 ausgabefeld = Label(fenster, background=farbstil)
 
-# Hier kann der Benutzer eine Eingabe machen
+# Eingabefeld für die Berechnung
 eingabefeld = Entry(fenster, bd=5, width=40, background=farbstil)
 
-welcom_button = Button(fenster, text="Berechnen", command=button_action, background=farbstil)
+#Buttons für Berechnung und Beenden erstellen
+berechnen_button = Button(fenster, text="Berechnen", command=button_action, background=farbstil)
 exit_button = Button(fenster, text="Beenden", command=fenster.quit, background=farbstil)
 
 
-# Nun fügen wir die Komponenten unserem Fenster hinzu
+# Aufgabei und ausrichtung der Fenster row/column = Zeile und Spalte. padx/pady= Abstand zu anderen Elementen.
 my_label.grid(row = 0, column = 0)
 eingabefeld.grid(row = 0, column = 1)
-welcom_button.grid(row = 2, column = 0)
+berechnen_button.grid(row = 2, column = 0)
 exit_button.grid(row = 2, column = 1, padx=10, pady=10)
 ausgabefeld.grid(row = 3, column = 0, columnspan = 2)
+
 
 mainloop()
